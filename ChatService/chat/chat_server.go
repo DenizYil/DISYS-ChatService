9aa3ce13b27330b36e3ddd8c5ece9519aa5d7c43
DenizYil <denizyil@outlook.dk>
2021-11-02 11:04:06 +0100

    Fix lamport stuff

--- ChatService/chat/chat_server.go
+++ ChatService/chat/chat_server.go
@@ -18,24 +18,22 @@ var lamport int32 = 0
 
 func (s *Server) Broadcast(ctx context.Context, message *Message) (*Empty, error) {
 	atomic.AddInt32(&lamport, 1)
-
-	message.Lamport = lamport
+	atomic.StoreInt32(&message.Lamport, lamport)
 
 	for _, client := range clients {
 		client.Send(message)
 	}
 
+	if message.User != "" {
+		log.Printf("(%s, %s) >> %s", strconv.Itoa(int(message.Lamport)), message.User, message.Content)
+	} else {
+		log.Printf("(%s) >> %s", strconv.Itoa(int(message.Lamport)), message.Content)
+	}
 	return &Empty{}, nil
 }
 
 func (s *Server) Join(message *JoinMessage, stream ChatService_JoinServer) error {
-	log.Printf("%s has connected to the chat!", message.User)
-
 	clients = append(clients, stream)
-
-	atomic.AddInt32(&lamport, 1)
-
-	stream.Send(&Message{User: "", Content: "Welcome " + message.User + "! You have connected to the chat!", Lamport: lamport})
 
 	msg := Message{
 		User:    "",
@@ -67,7 +65,6 @@ func (s *Server) Join(message *JoinMessage, stream ChatService_JoinServer) error
 }
 
 func (s *Server) Publish(ctx context.Context, message *Message) (*Empty, error) {
-	log.Printf("(%s, %s) >> %s", strconv.Itoa(int(message.Lamport)), message.User, message.Content)
 	atomic.StoreInt32(&lamport, MaxInt(lamport, message.Lamport)+1)
 	return s.Broadcast(ctx, message)
 }
